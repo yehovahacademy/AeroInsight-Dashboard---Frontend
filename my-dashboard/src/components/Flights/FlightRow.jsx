c69fdaa8665cf173c3c25c9ae8a2d8081b5503ed
yehovahacademy <yehovahacademy@gmail.com>
2026-06-29 10:53:20 +0530

    more changes

--- my-dashboard/src/components/Flights/FlightRow.jsx
+++ my-dashboard/src/components/Flights/FlightRow.jsx
@@ -9,6 +9,18 @@ import {faGem} from "@fortawesome/free-solid-svg-icons";
 import {faBell} from "@fortawesome/free-solid-svg-icons";
 
 
+import indigo from "../../assets/indigo.png";
+import akasa from "../../assets/akasa.png";
+import alaska from "../../assets/alaska.png";
+import alliance from "../../assets/alliance.png";
+import canada from "../../assets/canada.png";
+import delta from "../../assets/delta.png";
+import lufthansa from "../../assets/lufthansa.png";
+import nippon from "../../assets/nippon.png";
+import qatar from "../../assets/qatar.png";
+import express from "../../assets/express.png";
+
+
 
   function FlightRow() {
 
@@ -77,7 +89,7 @@ import {faBell} from "@fortawesome/free-solid-svg-icons";
 
     <br></br><br></br>
 
-     <div className="heading">
+     <div className="heading-hero">
       <h1>Do more with AeroInsight</h1>
     </div>
     <br></br>
@@ -120,6 +132,55 @@ import {faBell} from "@fortawesome/free-solid-svg-icons";
   <div className="fare-alerts">
     <FontAwesomeIcon icon={faBell} />
     <span>Fare Alerts</span>
+  </div>
+</div>
+
+<br></br> <br></br>
+<div className="hero-3">
+  <div className="airlines">
+    <div className="heading-airlines">Popular Airlines to Travel with</div>
+    <div className="airlines-grid">
+      <div className="airline-item">
+        <img src={indigo} alt="Indigo Airlines" />
+        <h2>Indigo Airlines</h2>
+      </div>
+      <div className="airline-item">
+        <img src={akasa} alt="Akasa Airlines" />
+        <h2>Akasa Airlines</h2>
+      </div>
+      <div className="airline-item">
+        <img src={alaska} alt="Alaska Airlines" />
+        <h2>Alaska Airlines</h2>
+      </div>
+      <div className="airline-item">
+        <img src={alliance} alt="Alliance Airlines" />
+        <h2>Alliance Airlines</h2>
+      </div>
+      <div className="airline-item">
+        <img src={canada} alt="Canada Airlines" />
+        <h2>Canada Airlines</h2>
+      </div>
+      <div className="airline-item">
+        <img src={delta} alt="Delta Airlines" />
+        <h2>Delta Airlines</h2>
+      </div>
+      <div className="airline-item">
+        <img src={lufthansa} alt="Lufthansa Airlines" />
+        <h2>Lufthansa Airlines</h2>
+      </div>
+      <div className="airline-item">
+        <img src={nippon} alt="Nippon Airlines" />
+        <h2>Nippon Airlines</h2>
+      </div>
+      <div className="airline-item">
+        <img src={qatar} alt="Qatar Airlines" />
+        <h2>Qatar Airways</h2>
+      </div>
+      <div className="airline-item">
+        <img src={express} alt="Air India Express" />
+        <h2>Air India Express</h2>
+      </div>
+    </div>
   </div>
 </div>
 
