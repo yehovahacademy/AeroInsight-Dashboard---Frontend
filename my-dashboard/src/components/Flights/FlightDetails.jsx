f320cfd8f6c884c8d84d0f63b71fb99465d52274
yehovahacademy <yehovahacademy@gmail.com>
2026-07-21 09:14:07 +0530

    New changes

--- my-dashboard/src/components/Flights/FlightDetails.jsx
+++ my-dashboard/src/components/Flights/FlightDetails.jsx
@@ -6,7 +6,7 @@ function FlightDetail() {
   const[flights, setFlights] = useState([]);
 
   useEffect(() => {
-    fetch("http://127.0.0.1:8000/flights")
+    fetch("https://aeroinsight-dashboard-backend.onrender.com/flights")
       .then((response) => response.json())
       .then((data) => setFlights(data))
       .catch((error) => console.error("Error fetching flights:", error));
--- my-dashboard/src/components/Flights/Home.jsx
+++ my-dashboard/src/components/Flights/Home.jsx
@@ -8,18 +8,7 @@ import { faUsers } from "@fortawesome/free-solid-svg-icons";
 import {faGem} from "@fortawesome/free-solid-svg-icons";
 import {faBell} from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
-
-
-import indigo from "../../assets/indigo.png";
-import akasa from "../../assets/akasa.png";
-import alaska from "../../assets/alaska.png";
-import alliance from "../../assets/alliance.png";
-import canada from "../../assets/canada.png";
-import delta from "../../assets/delta.png";
-import lufthansa from "../../assets/lufthansa.png";
-import nippon from "../../assets/nippon.png";
-import qatar from "../../assets/qatar.png";
-import express from "../../assets/express.png";
+import AirLineCards from "./AirLineCards";
 
 
 
@@ -148,54 +137,8 @@ const handleSearch = () => {
 </div>
 
 <br></br> <br></br>
-<div className="hero-3">
-  <div className="airlines">
-    <div className="heading-airlines">Popular Airlines to Travel with</div>
-    <div className="airlines-grid">
-      <div className="airline-item">
-        <img src={indigo} alt="Indigo Airlines" />
-        <h2>Indigo Airlines</h2>
-      </div>
-      <div className="airline-item">
-        <img src={akasa} alt="Akasa Airlines" />
-        <h2>Akasa Airlines</h2>
-      </div>
-      <div className="airline-item">
-        <img src={alaska} alt="Alaska Airlines" />
-        <h2>Alaska Airlines</h2>
-      </div>
-      <div className="airline-item">
-        <img src={alliance} alt="Alliance Airlines" />
-        <h2>Alliance Airlines</h2>
-      </div>
-      <div className="airline-item">
-        <img src={canada} alt="Canada Airlines" />
-        <h2>Canada Airlines</h2>
-      </div>
-      <div className="airline-item">
-        <img src={delta} alt="Delta Airlines" />
-        <h2>Delta Airlines</h2>
-      </div>
-      <div className="airline-item">
-        <img src={lufthansa} alt="Lufthansa Airlines" />
-        <h2>Lufthansa Airlines</h2>
-      </div>
-      <div className="airline-item">
-        <img src={nippon} alt="Nippon Airlines" />
-        <h2>Nippon Airlines</h2>
-      </div>
-      <div className="airline-item">
-        <img src={qatar} alt="Qatar Airlines" />
-        <h2>Qatar Airways</h2>
-      </div>
-      <div className="airline-item">
-        <img src={express} alt="Air India Express" />
-        <h2>Air India Express</h2>
-      </div>
-    </div>
-  </div>
-</div>
 
+<AirLineCards />
     <br></br>
 
 
