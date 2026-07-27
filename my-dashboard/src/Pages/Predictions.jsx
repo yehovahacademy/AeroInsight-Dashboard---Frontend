d6218e4da22d526caa9d3d7f852c5ee1c8621b93
yehovahacademy <yehovahacademy@gmail.com>
2026-07-27 14:42:04 +0530

    New designs and changes added and files as well

--- my-dashboard/src/Pages/Predictions.jsx
+++ my-dashboard/src/Pages/Predictions.jsx
@@ -1,18 +1,19 @@
 
 import  { useState, useEffect } from "react";
 import WeatherForecastCard from "../components/Predictions/WeatherForecastCard";
+import AviationRiskCard from "../components/Predictions/AviationRiskCard";
 
 
 
 
 function Predictions() {
 
-  const[weather, setWeather] = useState(null);
+  const [predictionData, setPredictionData] = useState(null);
 
   useEffect(() => {
     fetch( "https://aeroinsight-dashboard-backend.onrender.com/weather/?airport=BOM")
       .then((response) => response.json())
-      .then((data) => setWeather(data))
+      .then((data) => setPredictionData(data))
       .catch((error) => console.error("Error fetching weather data:", error));
   }, []);
 
@@ -20,7 +21,11 @@ function Predictions() {
     <>
     <br></br><br></br>
     
-    <WeatherForecastCard weather={weather} />  
+    <WeatherForecastCard weather= {predictionData} />  
+    <br></br><br></br>
+    <AviationRiskCard
+    risk={predictionData?.aviation_risk}
+/>
 
     <br></br><br></br>
 
