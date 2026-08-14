a4d4eb2822cf5bca93cb085e8f6812c419a1acb6
yehovahacademy <yehovahacademy@gmail.com>
2026-08-14 10:24:46 +0530

    Network Planner task completed

--- my-dashboard/src/Pages/RoutePlanner.jsx
+++ my-dashboard/src/Pages/RoutePlanner.jsx
@@ -3,21 +3,7 @@ import { Route,Routes } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 
-function RoutePlanner(){
-  const[route, setRoute] = useState(null);
-
-  useEffect(() => {
-    fetch("https://aeroinsight-dashboard-backend.onrender.com/airports/api/airports/search/QUO")
-      .then((response) => response.json())
-      .then((data) => {
-        setRoute(data); 
-      })
-      .catch((error) => {
-        console.error("Error fetching route data:", error);
-      });
-  }, []);  
-
-
+function RoutePlanner(){ 
   return (
     <>
     <NetworkPlanner />
