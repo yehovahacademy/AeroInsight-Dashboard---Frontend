f1de29101162b82bab8d3c2ea968104bb172df53
yehovahacademy <yehovahacademy@gmail.com>
2026-08-19 13:39:13 +0530

    The errors caused due to name changes is resolved

--- my-dashboard/src/App.jsx
+++ my-dashboard/src/App.jsx
@@ -1,10 +1,9 @@
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Dashboard from "./Pages/Dashboard";
+import Home from "./Pages/Home";
 import { Route,Routes } from "react-router-dom";
 import RoutePlanner from "./Pages/RoutePlanner";
 import Analytics from "./Pages/Analytics";
-import Home from "./components/Network/NetworkPlanner";
 import Predictions from "./Pages/Predictions";
 
 
@@ -21,7 +20,7 @@ function App() {
 
 <Routes>
    <Route path="/" element={<Home />} />
-  <Route path="/dashboard" element={<Dashboard />} />
+  <Route path="/home" element={<Dashboard />} />
   <Route path="/routeplanner" element={<RoutePlanner/>}/>
   <Route path ="/analytics" element={<Analytics/>}/>
   <Route path="/predictions" element={<Predictions/>}/>
