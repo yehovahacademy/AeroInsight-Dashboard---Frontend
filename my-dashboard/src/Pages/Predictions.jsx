cf50014a0bdf0633d5a830bd6ebd5576334ce5f4
yehovahacademy <yehovahacademy@gmail.com>
2026-08-25 10:46:01 +0530

    Added Demand Inetelligennce prop

--- my-dashboard/src/Pages/Predictions.jsx
+++ my-dashboard/src/Pages/Predictions.jsx
@@ -1,12 +1,17 @@
 import NetworkMap from "../components/Maps/NetworkMap"
+import DemandIntelligence from "../components/Demand Intelligence/DemandIntelligence"
 
 
 export default function Predictions() {
   return (
+    <>
     <div>
       <h1>Predictions Page</h1>
       <NetworkMap />
+      <br></br> <br></br>
       <h1>Market Demand</h1>
-    </div>
+      <DemandIntelligence />  
+      </div>
+      </>
   );
 }
