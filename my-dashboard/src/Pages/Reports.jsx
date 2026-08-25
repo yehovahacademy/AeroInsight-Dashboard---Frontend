853f557cd1177e0b0311e78901c2efcb0ae0229e
yehovahacademy <yehovahacademy@gmail.com>
2026-08-25 11:51:07 +0530

    Will also start with Reports page

---
+++ my-dashboard/src/Pages/Reports.jsx
@@ -0,0 +1,12 @@
+export default function Reports() {
+  useEffect(() => {
+    document.title = "AeroInsight | Reports"
+  })
+  return (
+    <>
+    <div className="heading-section">
+      <h1>Reports Page</h1>
+    </div>
+    </>
+  );
+}
