f4c31b0d5a3d80e1c0ed6957050592bb7726fcef
yehovahacademy <yehovahacademy@gmail.com>
2026-09-24 12:38:18 +0530

    Removing PRedictions and Reports page

--- my-dashboard/src/components/Navbar.jsx
+++ my-dashboard/src/components/Navbar.jsx
@@ -16,8 +16,6 @@ function Navbar() {
 
       <Link to="/Home">Home</Link>
       <Link to="/RoutePlanner">Route Planner</Link>
-      <Link to="/Predictions">Predictions</Link>
-      <Link to="/Reports">Reports</Link>
     </div>
     </>
   );
